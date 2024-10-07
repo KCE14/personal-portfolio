08a65d2a86c16556b20a17cd515d0abea5fa569c
Kelechi144 <ezechristian26@vt.edu>
2024-10-07 02:50:56 -0400

    added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,8 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/E.png";
 import projImg2 from "../assets/img/ec21.jpg";
 import projImg3 from "../assets/img/Screenshot (52).png";
+import projImg4 from "../assets/img/scripting.jpeg";
+import projImg5 from "../assets/img/medium-art.png";
 
 
 export const Projects = () => {
@@ -26,6 +28,22 @@ export const Projects = () => {
             link: "https://github.com/KCE14/CYB102-Milestone"
         },
     ];
+
+    const projects2 = [
+        {
+            title: "Scripting Exercises",
+            description: "Threat Automation and Scripting",
+            imgUrl: projImg4,
+            link: "https://github.com/KCE14/Scripting-Exercises"
+        },
+
+        {
+            title: "Medium Writeups",
+            description: "General Security and OSINT",
+            imgUrl: projImg5,
+            link: "https://github.com/KCE14/medium-writeups"
+        },
+    ]
 
     return (
         <section className="project" id="projects">
@@ -55,7 +73,11 @@ export const Projects = () => {
                                     </Row>
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="second">
-                                    <p>Stay tuned!</p>
+                                    <Row>
+                                        {projects2.map((project, index) => (
+                                            <ProjectCard key={index} {...project} />
+                                        ))}
+                                    </Row>
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="third">
                                     <p>Stay tuned!</p>
