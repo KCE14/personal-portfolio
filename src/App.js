7db624429d476bf054fe8d0cd5530f683c477229
Kelechi144 <149338631+Kelechi144@users.noreply.github.com>
2025-07-17 01:02:51 -0400

    Vercel analytics

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Skills } from './components/Skills';
 import { Projects } from './components/Projects';
 import { Footer } from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Skills />
       <Projects />
       <Footer />
+      <Analytics />
     </div>
   );
 }
