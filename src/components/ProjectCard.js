c18f5f9c8cda101790a9e711718c15ab4edeeb2a
Kelechi144 <149338631+Kelechi144@users.noreply.github.com>
2025-11-12 00:59:55 -0500

    bug fixes

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,7 +5,8 @@ export const ProjectCard = ({ title, description, imgUrl, link }) => {
     <Col size={12} sm={6} md={4}>
       <a href={link} target="_blank" rel="noopener noreferrer">
       <div className="proj-imgbx">
-        <img src={imgUrl} alt="image URL" />
+        {/* Changed alt to use the title prop */}
+        <img src={imgUrl} alt={title} /> 
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
